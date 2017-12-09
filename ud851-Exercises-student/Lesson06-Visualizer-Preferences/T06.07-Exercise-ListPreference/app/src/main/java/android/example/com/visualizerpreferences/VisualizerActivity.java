490a0e8444e63f21b5491e48aa600b913139586b
Felipe Joglar <felipejoglar@gmail.com>
2017-12-09 20:12:09 +0100

    Complete T06.07-Exercise-ListPreference

--- ud851-Exercises-student/Lesson06-Visualizer-Preferences/T06.07-Exercise-ListPreference/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ ud851-Exercises-student/Lesson06-Visualizer-Preferences/T06.07-Exercise-ListPreference/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -48,7 +48,7 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
         setupPermissions();
     }
 
-    // TODO (4) Update setupSharedPreferences and onSharedPreferenceChanged to load the color
+    // COMPLETED (4) Update setupSharedPreferences and onSharedPreferenceChanged to load the color
     // from shared preferences. Call setColor, passing in the color you got
     private void setupSharedPreferences() {
         // Get all of the values from shared preferences to set it up
@@ -60,7 +60,8 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
         mVisualizerView.setShowTreble(sharedPreferences.getBoolean(getString(R.string.pref_show_treble_key),
                 getResources().getBoolean(R.bool.pref_show_treble_default)));
         mVisualizerView.setMinSizeScale(1);
-        mVisualizerView.setColor(getString(R.string.pref_color_red_value));
+        mVisualizerView.setColor(sharedPreferences.getString(getString(R.string.pref_color_key),
+                getString(R.string.pref_color_red_value)));
         // Register the listener
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
@@ -73,6 +74,9 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
             mVisualizerView.setShowMid(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_mid_range_default)));
         } else if (key.equals(getString(R.string.pref_show_treble_key))) {
             mVisualizerView.setShowTreble(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_treble_default)));
+        } else if (key.equals(getString(R.string.pref_color_key))) {
+            mVisualizerView.setColor(sharedPreferences.getString(getString(R.string.pref_color_key),
+                    getString(R.string.pref_color_red_value)));
         }
     }
 
