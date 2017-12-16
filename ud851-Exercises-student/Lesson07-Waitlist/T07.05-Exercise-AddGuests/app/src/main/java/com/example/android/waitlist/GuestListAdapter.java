931a883e3b47ac9d9a6e4259130a5147428e2cb9
Felipe Joglar <felipejoglar@gmail.com>
2017-12-16 19:20:35 +0100

    Complete T07.05-Exercise-AddGuests

--- ud851-Exercises-student/Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ ud851-Exercises-student/Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -57,13 +57,22 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
         return mCursor.getCount();
     }
 
-    // TODO (15) Create a new function called swapCursor that takes the new cursor and returns void
+    // COMPLETED (15) Create a new function called swapCursor that takes the new cursor and returns void
+    public void swapCursor(Cursor newCursor) {
 
-    // TODO (16) Inside, check if the current cursor is not null, and close it if so
+        // COMPLETED (16) Inside, check if the current cursor is not null, and close it if so
+        if (mCursor != null) {
+            mCursor.close();
+        }
 
-    // TODO (17) Update the local mCursor to be equal to  newCursor
+        // COMPLETED (17) Update the local mCursor to be equal to  newCursor
+        mCursor = newCursor;
 
-    // TODO (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+        // COMPLETED (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+        if (newCursor != null) {
+            this.notifyDataSetChanged();
+        }
+    }
 
     /**
      * Inner class to hold the views needed to display a single item in the recycler-view
--- ud851-Exercises-student/Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ ud851-Exercises-student/Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -21,9 +21,12 @@ public class MainActivity extends AppCompatActivity {
     private GuestListAdapter mAdapter;
     private SQLiteDatabase mDb;
 
-    // TODO (1) Create local EditText members for mNewGuestNameEditText and mNewPartySizeEditText
+    // COMPLETED (1) Create local EditText members for mNewGuestNameEditText and mNewPartySizeEditText
+    private EditText mNewGuestNameEditText;
+    private EditText mNewPartySizeEditText;
 
-    // TODO (13) Create a constant string LOG_TAG that is equal to the class.getSimpleName()
+    // COMPLETED (13) Create a constant string LOG_TAG that is equal to the class.getSimpleName()
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +38,9 @@ public class MainActivity extends AppCompatActivity {
         // Set local attributes to corresponding views
         waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
 
-        // TODO (2) Set the Edit texts to the corresponding views using findViewById
+        // COMPLETED (2) Set the Edit texts to the corresponding views using findViewById
+        mNewGuestNameEditText = (EditText) findViewById(R.id.person_name_edit_text);
+        mNewPartySizeEditText = (EditText) findViewById(R.id.party_count_edit_text);
 
         // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -48,8 +53,7 @@ public class MainActivity extends AppCompatActivity {
         // because you will be adding restaurant customers
         mDb = dbHelper.getWritableDatabase();
 
-        // TODO (3) Remove this fake data call since we will be inserting our own data now
-        TestUtil.insertFakeData(mDb);
+        // COMPLETED (3) Remove this fake data call since we will be inserting our own data now
 
         // Get all guest info from the database and save in a cursor
         Cursor cursor = getAllGuests();
@@ -69,20 +73,33 @@ public class MainActivity extends AppCompatActivity {
      */
     public void addToWaitlist(View view) {
 
-        // TODO (9) First thing, check if any of the EditTexts are empty, return if so
+        // COMPLETED (9) First thing, check if any of the EditTexts are empty, return if so
+        if (mNewPartySizeEditText.getText().toString().equals(  "") ||
+                mNewGuestNameEditText.getText().toString().equals("")) {
+            return;
+        }
 
-        // TODO (10) Create an integer to store the party size and initialize to 1
+        // COMPLETED (10) Create an integer to store the party size and initialize to 1
+        int partySize = 1;
 
-        // TODO (11) Use Integer.parseInt to parse mNewPartySizeEditText.getText to an integer
+        // COMPLETED (11) Use Integer.parseInt to parse mNewPartySizeEditText.getText to an integer
+        // COMPLETED (12) Make sure you surround the Integer.parseInt with a try catch and log any exception
+        try {
+            partySize = Integer.parseInt(mNewPartySizeEditText.getText().toString());
+        } catch (NumberFormatException ex) {
+            Log.d(LOG_TAG, ex.toString());
+        }
 
-        // TODO (12) Make sure you surround the Integer.parseInt with a try catch and log any exception
+        // COMPLETED (14) call addNewGuest with the guest name and party size
+        addGuest(mNewGuestNameEditText.getText().toString(), partySize);
 
-        // TODO (14) call addNewGuest with the guest name and party size
+        // COMPLETED (19) call mAdapter.swapCursor to update the cursor by passing in getAllGuests()
+        mAdapter.swapCursor(getAllGuests());
 
-        // TODO (19) call mAdapter.swapCursor to update the cursor by passing in getAllGuests()
-
-        // TODO (20) To make the UI look nice, call .getText().clear() on both EditTexts, also call clearFocus() on mNewPartySizeEditText
-
+        // COMPLETED (20) To make the UI look nice, call .getText().clear() on both EditTexts, also call clearFocus() on mNewPartySizeEditText
+        mNewPartySizeEditText.clearFocus();
+        mNewPartySizeEditText.getText().clear();
+        mNewGuestNameEditText.getText().clear();
     }
 
 
@@ -104,16 +121,20 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
-    // TODO (4) Create a new addGuest method
+    // COMPLETED (4) Create a new addGuest method
+    private void addGuest(String name, int partySize) {
+        // COMPLETED (5) Inside, create a ContentValues instance to pass the values onto the insert query
+        ContentValues cv = new ContentValues();
 
-    // TODO (5) Inside, create a ContentValues instance to pass the values onto the insert query
+        // COMPLETED (6) call put to insert the name value with the key COLUMN_GUEST_NAME
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME, name);
 
-    // TODO (6) call put to insert the name value with the key COLUMN_GUEST_NAME
+        // COMPLETED (7) call put to insert the party size value with the key COLUMN_PARTY_SIZE
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE, partySize);
 
-    // TODO (7) call put to insert the party size value with the key COLUMN_PARTY_SIZE
-
-    // TODO (8) call insert to run an insert query on TABLE_NAME with the ContentValues created
-
+        // COMPLETED (8) call insert to run an insert query on TABLE_NAME with the ContentValues created
+        mDb.insert(WaitlistContract.WaitlistEntry.TABLE_NAME, null, cv);
+    }
 
 
 }
