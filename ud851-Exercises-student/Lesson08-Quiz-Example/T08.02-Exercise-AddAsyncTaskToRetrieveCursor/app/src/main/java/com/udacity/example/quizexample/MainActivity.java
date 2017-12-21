daba8f886f156641c0a75179bac2aaec7f7ad765
Felipe Joglar <felipejoglar@gmail.com>
2017-12-21 20:04:25 +0100

    Complete T08.02-Exercise-AddAsyncTaskToRetrieveCursor

--- ud851-Exercises-student/Lesson08-Quiz-Example/T08.02-Exercise-AddAsyncTaskToRetrieveCursor/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ ud851-Exercises-student/Lesson08-Quiz-Example/T08.02-Exercise-AddAsyncTaskToRetrieveCursor/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -16,10 +16,15 @@
 
 package com.udacity.example.quizexample;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
 /**
  * Gets the data from the ContentProvider and shows a series of flash cards.
@@ -30,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
     // The current state of the app
     private int mCurrentState;
 
-    // TODO (3) Create an instance variable storing a Cursor called mData
+    // COMPLETED (3) Create an instance variable storing a Cursor called mData
+    private Cursor mCursor;
+
     private Button mButton;
 
     // This state is when the word definition is hidden and clicking the button will therefore
@@ -50,12 +57,14 @@ public class MainActivity extends AppCompatActivity {
         // Get the views
         mButton = (Button) findViewById(R.id.button_next);
 
-        // TODO (5) Create and execute your AsyncTask here
+        // COMPLETED (5) Create and execute your AsyncTask here
+        new AccessDroidTermsTask().execute();
     }
 
     /**
      * This is called from the layout when the button is clicked and switches between the
      * two app states.
+     *
      * @param view The view that was clicked
      */
     public void onButtonClick(View view) {
@@ -90,9 +99,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // TODO (1) Create AsyncTask with the following generic types <Void, Void, Cursor>
-    // TODO (2) In the doInBackground method, write the code to access the DroidTermsExample
-    // provider and return the Cursor object
-    // TODO (4) In the onPostExecute method, store the Cursor object in mData
+    // COMPLETED (1) Create AsyncTask with the following generic types <Void, Void, Cursor>
+    private class AccessDroidTermsTask extends AsyncTask<Void, Void, Cursor> {
+
+        // COMPLETED (2) In the doInBackground method, write the code to access the DroidTermsExample
+        // provider and return the Cursor object
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+            // Get the content resolver
+            ContentResolver resolver = getContentResolver();
+
+            // Call the query method on the resolver with the correct Uri from the contract class
+            Cursor cursor = resolver.query(DroidTermsExampleContract.CONTENT_URI,
+                    null, null, null, null);
+            return cursor;
+        }
+
+        // COMPLETED (4) In the onPostExecute method, store the Cursor object in mData
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            super.onPostExecute(cursor);
+
+            mCursor = cursor;
+        }
+    }
 
 }
