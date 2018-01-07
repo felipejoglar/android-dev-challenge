3a998e7492c0909ff3bb86187547d3637e358dea
Felipe Joglar <felipejoglar@gmail.com>
2018-01-07 16:51:55 +0100

    Complete S10.04-Exercise-Notifications

--- ud851-Sunshine-student/S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ ud851-Sunshine-student/S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -37,9 +37,9 @@ public final class SunshinePreferences {
      * <p>
      * When the location details are updated, the database should to be cleared.
      *
-     * @param context  Context used to get the SharedPreferences
-     * @param lat      the latitude of the city
-     * @param lon      the longitude of the city
+     * @param context Context used to get the SharedPreferences
+     * @param lat     the latitude of the city
+     * @param lon     the longitude of the city
      */
     public static void setLocationDetails(Context context, double lat, double lon) {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
@@ -127,7 +127,7 @@ public final class SunshinePreferences {
          * into the double itself.
          */
         preferredCoordinates[0] = Double
-                 .longBitsToDouble(sp.getLong(PREF_COORD_LAT, Double.doubleToRawLongBits(0.0)));
+                .longBitsToDouble(sp.getLong(PREF_COORD_LAT, Double.doubleToRawLongBits(0.0)));
         preferredCoordinates[1] = Double
                 .longBitsToDouble(sp.getLong(PREF_COORD_LONG, Double.doubleToRawLongBits(0.0)));
 
@@ -153,6 +153,36 @@ public final class SunshinePreferences {
         }
 
         return spContainBothLatitudeAndLongitude;
+    }
+
+    /**
+     * Returns true if the user prefers to see notifications from Sunshine, false otherwise. This
+     * preference can be changed by the user within the SettingsFragment.
+     *
+     * @param context Used to access SharedPreferences
+     * @return true if the user prefers to see notifications, false otherwise
+     */
+    public static boolean areNotificationsEnabled(Context context) {
+        /* Key for accessing the preference for showing notifications */
+        String displayNotificationsKey = context.getString(R.string.pref_enable_notifications_key);
+
+        /*
+         * In Sunshine, the user has the ability to say whether she would like notifications
+         * enabled or not. If no preference has been chosen, we want to be able to determine
+         * whether or not to show them. To do this, we reference a bool stored in bools.xml.
+         */
+        boolean shouldDisplayNotificationsByDefault = context
+                .getResources()
+                .getBoolean(R.bool.show_notifications_by_default);
+
+        /* As usual, we use the default SharedPreferences to access the user's preferences */
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+
+        /* If a value is stored with the key, we extract it here. If not, use a default. */
+        boolean shouldDisplayNotifications = sp
+                .getBoolean(displayNotificationsKey, shouldDisplayNotificationsByDefault);
+
+        return shouldDisplayNotifications;
     }
 
     /**
@@ -202,7 +232,7 @@ public final class SunshinePreferences {
      * Saves the time that a notification is shown. This will be used to get the ellapsed time
      * since a notification was shown.
      *
-     * @param context Used to access SharedPreferences
+     * @param context            Used to access SharedPreferences
      * @param timeOfNotification Time of last notification to save (in UNIX time)
      */
     public static void saveLastNotificationTime(Context context, long timeOfNotification) {
--- ud851-Sunshine-student/S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/utilities/NotificationUtils.java
+++ ud851-Sunshine-student/S10.04-Exercise-Notifications/app/src/main/java/com/example/android/sunshine/utilities/NotificationUtils.java
@@ -1,14 +1,22 @@
 package com.example.android.sunshine.utilities;
 
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v4.content.ContextCompat;
 
+import com.example.android.sunshine.DetailActivity;
 import com.example.android.sunshine.R;
+import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
 
 public class NotificationUtils {
@@ -32,7 +40,8 @@ public class NotificationUtils {
     public static final int INDEX_MAX_TEMP = 1;
     public static final int INDEX_MIN_TEMP = 2;
 
-//  TODO (1) Create a constant int value to identify the notification
+    //  COMPLETED (1) Create a constant int value to identify the notification
+    private static final int WEATHER_NOTIFICATION_ID = 3004;
 
     /**
      * Constructs and displays a notification for the newly updated weather for today.
@@ -83,19 +92,46 @@ public class NotificationUtils {
             int smallArtResourceId = SunshineWeatherUtils
                     .getSmallArtResourceIdForWeatherCondition(weatherId);
 
-//          TODO (2) Use NotificationCompat.Builder to begin building the notification
+//          COMPLETED (2) Use NotificationCompat.Builder to begin building the notification
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
+                    .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                    .setSmallIcon(smallArtResourceId)
+                    .setLargeIcon(largeIcon)
+                    .setContentTitle(notificationTitle)
+                    .setContentText(notificationText)
+                    .setAutoCancel(true);
 
-//          TODO (3) Create an Intent with the proper URI to start the DetailActivity
+//          COMPLETED (3) Create an Intent with the proper URI to start the DetailActivity
+            /*
+             * This Intent will be triggered when the user clicks the notification. In our case,
+             * we want to open Sunshine to the DetailActivity to display the newly updated weather.
+             */
+            Intent detailIntentForToday = new Intent(context, DetailActivity.class);
+            detailIntentForToday.setData(todaysWeatherUri);
 
-//          TODO (4) Use TaskStackBuilder to create the proper PendingIntent
+//          COMPLETED (4) Use TaskStackBuilder to create the proper PendingINtent
+            TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+            taskStackBuilder.addNextIntentWithParentStack(detailIntentForToday);
+            PendingIntent resultPendingIntent = taskStackBuilder
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-//          TODO (5) Set the content Intent of the NotificationBuilder
+//          COMPLETED (5) Set the content Intent of the NotificationBuilder
+            notificationBuilder.setContentIntent(resultPendingIntent);
 
-//          TODO (6) Get a reference to the NotificationManager
+//          COMPLETED (6) Get a reference to the NotificationManager
+            NotificationManager notificationManager = (NotificationManager)
+                    context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-//          TODO (7) Notify the user with the ID WEATHER_NOTIFICATION_ID
+//          COMPLETED (7) Notify the user with the ID WEATHER_NOTIFICATION_ID
+            /* WEATHER_NOTIFICATION_ID allows you to update or cancel the notification later on */
+            notificationManager.notify(WEATHER_NOTIFICATION_ID, notificationBuilder.build());
 
-//          TODO (8) Save the time at which the notification occurred using SunshinePreferences
+//          COMPLETED (8) Save the time at which the notification occurred using SunshinePreferences
+            /*
+             * Since we just showed a notification, save the current time. That way, we can check
+             * next time the weather is refreshed if we should show another notification.
+             */
+            SunshinePreferences.saveLastNotificationTime(context, System.currentTimeMillis());
         }
 
         /* Always close your cursor when you're done with it to avoid wasting resources. */
