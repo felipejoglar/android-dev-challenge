0008cf357be73a7fded93d9ae0cf994cd2de4b42
Felipe Joglar <felipejoglar@gmail.com>
2018-01-07 13:14:22 +0100

    Complete T10.01-Exercise-IntentServices

--- ud851-Exercises-student/Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ ud851-Exercises-student/Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,14 +1,29 @@
 package com.example.android.background.sync;
 
-// TODO (1) Create a class called ReminderTasks
+import android.content.Context;
 
-// TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+import com.example.android.background.utilities.PreferenceUtilities;
 
-// TODO (6) Create a public static void method called executeTask
-// TODO (7) Add a Context called context and String parameter called action to the parameter list
+// COMPLETED (1) Create a class called ReminderTasks
+public class ReminderTasks{
 
-// TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+    // COMPLETED (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+    public static final String ACTION_INCREMENT_WATER_COUNT = "action_increment_water_count";
 
-// TODO (3) Create a private static void method called incrementWaterCount
-// TODO (4) Add a Context called context to the argument list
-// TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+    // COMPLETED (6) Create a public static void method called executeTask
+    // COMPLETED (7) Add a Context called context and String parameter called action to the parameter list
+    public static void executeTask(Context context, String action) {
+        // COMPLETED (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+        if (action.equals(ACTION_INCREMENT_WATER_COUNT)) {
+            incrementWaterCount(context);
+        }
+    }
+
+    // COMPLETED (3) Create a private static void method called incrementWaterCount
+    // COMPLETED (4) Add a Context called context to the argument list
+    private static void incrementWaterCount(Context context) {
+        // COMPLETED (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+
+}
--- ud851-Exercises-student/Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ ud851-Exercises-student/Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -1,3 +1,4 @@
+package com.example.android.background.sync;
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -14,10 +15,25 @@
  * limitations under the License.
  */
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+// COMPLETED (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+//  COMPLETED (10) Create a default constructor that calls super with the name of this class
+    public WaterReminderIntentService() {
+        super("WaterReminderIntentService");
+    }
+
+//  COMPLETED (11) Override onHandleIntent
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+//      COMPLETED (12) Get the action from the Intent that started this Service
+        String action = intent.getAction();
+
+//      COMPLETED (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+        ReminderTasks.executeTask(this, action);
+    }
+}
