4bf9d726d0efe66b43626933aab6ae4075bdf4ff
Felipe Joglar <felipejoglar@gmail.com>
2018-01-07 16:25:37 +0100

    Complete S10.01-Exercise-SynchronizingTheWeather

--- ud851-Sunshine-student/S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ ud851-Sunshine-student/S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,3 +1,22 @@
-// TODO (9) Create a class called SunshineSyncUtils
-    //  TODO (10) Create a public static void method called startImmediateSync
-    //  TODO (11) Within that method, start the SunshineSyncIntentService
+package com.example.android.sunshine.sync;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+// COMPLETED (9) Create a class called SunshineSyncUtils
+public class SunshineSyncUtils {
+    //  COMPLETED (10) Create a public static void method called startImmediateSync
+
+    /**
+     * Helper method to perform a sync immediately using an IntentService for asynchronous
+     * execution.
+     *
+     * @param context The Context used to start the IntentService for the sync.
+     */
+    public static void startImmediateSync(@NonNull final Context context) {
+//      COMPLETED (11) Within that method, start the SunshineSyncIntentService
+        Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(intentToSyncImmediately);
+    }
+}
